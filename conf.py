ba45bcfa8931a38a12a9a2c68292ce46a5c5cb69
Filip Beskyd <beskyfil@fit.cvut.cz>
2020-01-20 17:45:59 +0100

    read env auth config

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 class Config:
     def __init__(self, _cfg_file_name):
@@ -6,16 +7,23 @@ class Config:
         self.config = configparser.ConfigParser()
         self.config.read(_cfg_file_name)
         self.check_config()
+        self.auth_conf = os.environ['AUTH_CONFIG']
     
     def check_config(self):
         if not all(s in ['labels', 'repos'] for s in self.config.sections()):
             raise RuntimeError('config error')
+
+        if not os.getenv('AUTH_CONFIG'):
+            raise RuntimeError('no AUTH_CONFIG environment variable found')
 
     def get_labels_repo(self):
         return self.config['labels']['l']
 
     def get_repos_to_synch(self):
         return [self.config['repos'][r] for r in self.config['repos']]
+
+    def get_github_login(self):
+        return self.auth_conf.split(':')[0], self.auth_conf.split(':')[1]
 
 c = Config('cfg.cfg')
 print(c.get_repos_to_synch())
