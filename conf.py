4349313d67e5a624d80a99c5fe7e7b04680ee0a3
Filip Beskyd <beskyfil@fit.cvut.cz>
2020-01-21 01:13:55 +0100

    rename method

--- conf.py
+++ conf.py
@@ -16,12 +16,13 @@ class Config:
         if not os.getenv('AUTH_CONFIG'):
             raise RuntimeError('no AUTH_CONFIG environment variable set')
 
-    def get_labels_repo(self):
+    def get_repo_with_labels(self):
         return self.config['labels']['l']
 
     def get_repos_to_synch(self):
         return [self.config['repos'][r] for r in self.config['repos']]
 
+    # username:token
     def get_github_login(self):
         return self.auth_conf.split(':')[0], self.auth_conf.split(':')[1]
 
