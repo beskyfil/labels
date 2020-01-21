0627edb1db3137c0408198669474321a9120a97a
Filip Beskyd <beskyfil@fit.cvut.cz>
2020-01-21 18:06:38 +0100

    implemeted method which returns mandatory labels as JSON

--- sync.py
+++ sync.py
@@ -1,10 +1,21 @@
 from flask import Flask, escape, request
 from conf import Config
+from github import Github
+import requests
 
 app = Flask(__name__)
 
-def get_labels():
-    pass
+c = Config('cfg.cfg')
+github = Github(c)
+
+def get_labels(labels_loc):
+    print(labels_loc)
+    _, owner, repo = labels_loc.split('/')
+    r = requests.get(f'{github.api_url}/{owner}/{repo}/labels')
+    labels = r.json()
+    return labels
+
+required_labels = get_labels(c.get_repo_with_labels())
 
 @app.route('/')
 def hello():
