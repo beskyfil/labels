2872bbc060e0a48ee228262fbe3eefa65b35b638
Filip Beskyd <beskyfil@fit.cvut.cz>
2020-01-21 01:39:49 +0100

    github now "implements" abstract service

--- github.py
+++ github.py
@@ -1,5 +1,12 @@
-class Github:
+from service import Service
+
+class Github(Service):
     def __init__(self, login):
         self.login = login
         self.api_url = 'https://api.github.com/repos/'
         # f'https://api.github.com/repos/{self.reposlug}/issues'
+
+    def abc(self):
+        pass
+
+g = Github('abc')
