2b0824a75d5d5d32bd6dd0f4720428ca00a22594
Filip Beskyd <beskyfil@fit.cvut.cz>
2020-01-21 13:47:47 +0100

    communication method, wrapper for github API

--- github.py
+++ github.py
@@ -1,12 +1,21 @@
 from service import Service
+import requests
 
 class Github(Service):
     def __init__(self, login):
-        self.login = login
+        self.login, self.token = login
         self.api_url = 'https://api.github.com/repos/'
         # f'https://api.github.com/repos/{self.reposlug}/issues'
+        self.communication()
 
-    def abc(self):
+    def update_labels(self):
         pass
 
-g = Github('abc')
+    def communication(self):
+        self.session = requests.Session()
+        self.session.headers = {'User-Agent': 'Github label sync bot', 'Authorization' : f'token {self.token}'}
+
+        #check if i can do somethign with this repo
+        r = self.session.get(f'https://api.github.com/repos/{self.reposlug}/issues')
+        if r.status_code != 200:
+            raise RuntimeError('invalid repo')
