da71cb96b5bc13c3c626054dc8a815f708908fa9
Filip Beskyd <beskyfil@fit.cvut.cz>
2020-01-27 17:11:14 +0100

    docstrings

--- helpers.py
+++ helpers.py
@@ -2,11 +2,20 @@ import hashlib
 import hmac
 
 def check_github_hook(hook, secret):
-        if not "X-Hub-Signature" in hook.headers:
-            return "OK, but unsafe", 200
-        request_signature = hook.headers["X-Hub-Signature"].split('=')
-        secret = secret.encode("utf-8")
-        digest = hmac.new(secret, hook.data, hashlib.sha1).hexdigest()
-        if len(request_signature) < 2 or request_signature[0] != 'sha1' or not hmac.compare_digest(request_signature[1], digest):
-            return 'Invalid signature!', 400
-        return 'OK, valid signature', 200
+    """
+    Checks integrity of the webhook which app receives from GITHUB,
+    it computes HMAC.
+
+    :param hook: Hook with its original content including header and payload
+    :param secret: Secret string which was included when creating webhook, it's used in HMAC
+    :return: tuple of (string, integer) where string is verbose interpretation of integer status code
+    """
+
+    if not "X-Hub-Signature" in hook.headers:
+        return "OK, but unsafe", 200
+    request_signature = hook.headers["X-Hub-Signature"].split('=')
+    secret = secret.encode("utf-8")
+    digest = hmac.new(secret, hook.data, hashlib.sha1).hexdigest()
+    if len(request_signature) < 2 or request_signature[0] != 'sha1' or not hmac.compare_digest(request_signature[1], digest):
+        return 'Invalid signature!', 400
+    return 'OK, valid signature', 200
--- service.py
+++ service.py
@@ -1,12 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
 class Service(metaclass=ABCMeta):
+    """Abstract class of service this label-syncing app supports,
+    this is just to make sure you dont forget to implement any of required methods"""
     @abstractmethod
     def __init__(self, config):
         pass
 
     @abstractmethod
-    def update_labels(self, repo):
+    def update_labels(self):
         pass
 
     @abstractmethod
